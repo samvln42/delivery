5e8d2f4ca200af2212f8cdccc8fafaf3892db9ed
seonekr <145427113+seonekr@users.noreply.github.com>
2024-09-20 14:28:10 +0700

    7 commit

--- oversea_backend/settings.py
+++ oversea_backend/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["43.201.158.188", "findfoodlao.com", "findfoodlao", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["52.79.215.241", "localhost", "127.0.0.1"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
